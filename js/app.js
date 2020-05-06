2f1c4709b1b31d2a2d681294bee8438f72496394
LuisDet <luisedenist@gmail.com>
2020-05-06 13:39:03 -0400

    Pagina completa de Unified

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// let count = 0
+
+// let timer = 0
+
+
+// function sliderTimer(){
+//     const images = document.querySelectorAll('.slider img')
+//     const slider = document.querySelector('.slider')
+//     const size = images[0].clientWidth
+
+//     if(count > 4) count = 0
+
+
+//     slider.addEventListener('transitionend', ()=>{
+//         if(images[count].id === 'lastImage'){
+//             slider.style.transition = "none"
+//             count = 0
+//             slider.style.transform = `translateX(${-size * count}px)`
+//         }
+//     })
+    
+//     if(images[count] === images[3]){
+//         timer = 1000
+
+//     }
+//     else{
+//         timer = 10000
+//     }
+
+//     slider.style.transition = 'transform 0.5s ease-in-out'
+//     slider.style.transform = `translateX(${-size * count}px)`
+    
+//     count ++
+//     setTimeout(sliderTimer,timer);
+// } 
+
+// sliderTimer();
+
+const asideMenu = document.querySelector('.filtro_producto')
+
+window.addEventListener('scroll', (e)=>{
+    console.log(asideMenu.target)    
+})
